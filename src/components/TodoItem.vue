22ffc165723dddbd8baf40379f2221670ae23f33
dmitrievaek <e.dmitrieva@skai.online>
2025-01-05 18:09:35 +0300

    feat : Add checkbox in TodoItem

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="task">
-		<div>{{ task.name }}</div>
+		<label for="todo_pin" class="todo_app_pin" >
+			<input class="todo_pin_item" type="checkbox" id="todo_pin" />
+			<i class="todo_pin_icon"></i>
+		</label>
+		<label for="todo_checkbox" class="todo_app_checkbox" >
+			<input class="todo_checkbox_item" type="checkbox" id="todo_checkbox" />
+		</label>
+		<div class="todo_item_name">{{ task.name }}</div>
 		<my-button 
 			class="del_btn"
 			@click="$emit('remove', post)"
 		>
-			
 		</my-button>
 	</div>
 </template>
@@ -27,8 +33,51 @@ export default {
 .task {;
 	display:flex;
 	align-items:center;
-	justify-content:space-between;
 	padding: 0.3rem 2rem 0.5rem 2rem;
+}
+.todo_app_pin{
+
+}
+.todo_pin_item{
+	position: absolute;
+    width: 15px;
+    height: 15px;
+    opacity:0;
+    cursor: pointer;
+}
+.todo_pin_icon{
+	opacity:0;
+	width: 10px;
+    height: 10px;
+	display:inline-block;
+	position:relative;
+	margin-right:15px;
+	cursor:pointer;
+}
+.todo_pin_icon:hover{
+	opacity:1;
+}
+.todo_pin_icon:after {
+    content: "\25B2";
+	color:red;
+	display: inline-block;
+    font-size: 20px;
+}
+.todo_item_name{
+	margin-right: 1rem;
+    display: block;
+    width: 100%;
+    word-break: break-all;
+    font-size: 1rem;
+}
+.todo_app_checkbox{
+	margin-right: 1rem;
+	margin-top: 0.3rem;
+}
+.todo_checkbox_item{
+	width: 15px;
+    height: 15px;
+	cursor:pointer;
 }
 .del_btn{
 	position: relative;
